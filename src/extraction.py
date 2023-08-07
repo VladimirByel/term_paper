d1812243da2619667fc4f1a02d85c2aae5994cfb
Vladimir Byelov <vladimir.byeloff@gmail.com>
2023-08-07 16:11:01 +0300

    function to sort data and get rid of the empty dict

--- src/extraction.py
+++ src/extraction.py
@@ -1,6 +1,16 @@
 import json
 
 
-def json_thing():
+def get_json():
     with open("operations.json", "r", encoding="utf-8") as file:
         return json.load(file)
+
+
+def sort_data(data):
+    working_dicts = []
+    for datum in data:
+        if not datum:
+            continue
+        working_dicts.append(datum)
+    sorted_input = sorted(working_dicts, key=lambda u: u['date'])
+    return sorted_input
